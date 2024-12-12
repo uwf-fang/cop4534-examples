32c9a496ec8194cbe28221b47b5e7c128be2daf3
Ian Fang <xfang@uwf.edu>
2024-12-11 22:11:19 -0600

    small improvement

--- m02-empirical/timeit.cpp
+++ m02-empirical/timeit.cpp
@@ -1,11 +1,15 @@
 #include <chrono>
 #include <iostream>
 
+void algorithm(int parameter) {
+  // time consuming algorithm
+}
+
 int main() {
   // Start the clock
   auto start = std::chrono::high_resolution_clock::now();
   // Call the function to be measured
-  algorithm(parameter);
+  algorithm(10000);
   // Stop the clock
   auto end = std::chrono::high_resolution_clock::now();
   // Calculate the duration
